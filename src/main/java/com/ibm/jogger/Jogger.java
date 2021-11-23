0eb00b14a7b01947909fbf776a517733b6d1bc19
mikelo <michele.orlandi@gmail.com>
2021-11-23 18:19:07 +0100

    added dyanmic custom fields

--- src/main/java/com/ibm/jogger/Jogger.java
+++ src/main/java/com/ibm/jogger/Jogger.java
@@ -1,45 +1,54 @@
 package com.ibm.jogger;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
+import org.apache.logging.log4j.ThreadContext;
+import org.json.JSONObject;
 
 public class Jogger {
 
-    private static final Logger logger = LogManager.getLogger(Jogger.class);
+    private static final Logger logger = LogManager.getLogger();
 
     public static void main( String[] args ) {
 
-        logger.trace("Entering application.");
+        logger.info("test message");
         // Bar bar = new Bar();
         // if (!bar.doIt()) {
         //     logger.error("Didn't do it.");
         // }
-        // logger.info('{"level":"info","name":"fred","home":"bedrock"}');
-        // logger.
-        logger.error("forza PIELLE");
-        logger.trace("forza roma");
-        logger.warn("forza warn");
-        
 
-        Marker marker = MarkerManager.getMarker("CLASS");
-        Child child = new Child();
+        logger.error("test message");
+        logger.trace("test message");
+        logger.warn("test message");
+        ThreadContext.put("MSG", "message_id_test");
+        ThreadContext.put("TRN", "transaction_id_test");
+        // Map<String,String> msgMap = new HashMap<>();
+        // msgMap.put("MSG", "2292929");
+        // JSONObject message = new JSONObject(msgMap);
+        logger.error("test message");
+        // Marker marker = MarkerManager.getMarker("MESSID.2292929");
+        // Child child = new Child();
 
-        System.out.println("------- Parent Logger ----------");
-        child.log(null);
-        child.log(marker);
-        child.logFromChild(null);
-        child.logFromChild(marker);
-        child.parentLog(null);
-        child.parentLog(marker);
+        // System.out.println("------- Parent Logger ----------");
+        // child.log(null);
+        // child.log(marker);
+        // child.logFromChild(null);
+        // child.logFromChild(marker);
+        // child.parentLog(null);
+        // child.parentLog(marker);
 
-        child.setLogger(child.childLogger);
+        // child.setLogger(child.childLogger);
 
-        System.out.println("------- Parent Logger set to Child Logger ----------");
-        child.log(null);
-        child.log(marker);
-        child.logFromChild(null);
-        child.logFromChild(marker);
+        // System.out.println("------- Parent Logger set to Child Logger ----------");
+        // child.log(null);
+        // child.log(marker);
+        // child.logFromChild(null);
+        // child.logFromChild(marker);
     }
 }
